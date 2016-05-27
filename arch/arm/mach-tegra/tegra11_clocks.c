72bc10793b90d1f618a8d28221a89db25721dca5
tdro <tdro@github.com>
2016-05-27 00:04:29 -0400

    overclock tegra 4 to 2116 MHz

--- arch/arm/mach-tegra/tegra11_clocks.c
+++ arch/arm/mach-tegra/tegra11_clocks.c
@@ -6020,7 +6020,7 @@ static struct clk tegra_dfll_cpu = {
 	.flags     = DFLL,
 	.ops       = &tegra_dfll_ops,
 	.reg	   = 0x2f4,
-	.max_rate  = 2000000000,
+	.max_rate  = 2126500000,
 };
 
 static struct clk tegra_pll_re_vco = {
@@ -6305,7 +6305,7 @@ static struct clk tegra_clk_cclk_g = {
 	.inputs	= mux_cclk_g,
 	.reg	= 0x368,
 	.ops	= &tegra_super_ops,
-	.max_rate = 2000000000,
+	.max_rate = 2126500000,
 };
 
 static struct clk tegra_clk_cclk_lp = {
@@ -6330,7 +6330,7 @@ static struct clk tegra_clk_virtual_cpu_g = {
 	.name      = "cpu_g",
 	.parent    = &tegra_clk_cclk_g,
 	.ops       = &tegra_cpu_ops,
-	.max_rate  = 2000000000,
+	.max_rate  = 2126500000,
 	.u.cpu = {
 		.main      = &tegra_pll_x,
 		.backup    = &tegra_pll_p_out4,
@@ -6361,7 +6361,7 @@ static struct clk tegra_clk_cpu_cmplx = {
 	.name      = "cpu",
 	.inputs    = mux_cpu_cmplx,
 	.ops       = &tegra_cpu_cmplx_ops,
-	.max_rate  = 2000000000,
+	.max_rate  = 2126500000,
 };
 
 static struct clk tegra_clk_cop = {
